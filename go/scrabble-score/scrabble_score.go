503a8e333649f28e9da83d365abb73ed5d4f96c8
Carl Cliver <carl@carls-air.lan>
2018-08-27 23:40:50 -0400

    Refactoring to use switch and compile the regex before the conditional.

--- go/scrabble-score/scrabble_score.go
+++ go/scrabble-score/scrabble_score.go
@@ -1,37 +1,39 @@
 package scrabble
 
 import (
-  "strings"
-  "regexp"
+	"regexp"
+	"strings"
 )
 
 // Score computes the scrabble score of a word.
 func Score(word string) int {
-  score := 0
+	score := 0
+	onePoint := regexp.MustCompile("[aeioulnrst]")
+	twoPoint := regexp.MustCompile("[dg]")
+	threePoint := regexp.MustCompile("[bcmp]")
+	fourPoint := regexp.MustCompile("[fhvwy]")
+	fivePoint := regexp.MustCompile("[k]")
+	eightPoint := regexp.MustCompile("[jx]")
+	tenPoint := regexp.MustCompile("[qz]")
 
-  for _, ch := range strings.ToLower(word) {
-    if matched, _ := regexp.MatchString("[aeioulnrst]", string(ch)); matched {
-      score++
-    }
-    if matched, _ := regexp.MatchString("[dg]", string(ch)); matched {
-      score += 2
-    }
-    if matched, _ := regexp.MatchString("[bcmp]", string(ch)); matched {
-      score += 3
-    }
-    if matched, _ := regexp.MatchString("[fhvwy]", string(ch)); matched {
-      score += 4
-    }
-    if matched, _ := regexp.MatchString("[k]", string(ch)); matched {
-      score += 5
-    }
-    if matched, _ := regexp.MatchString("[jx]", string(ch)); matched {
-      score += 8
-    }
-    if matched, _ := regexp.MatchString("[qz]", string(ch)); matched {
-      score += 10
-    }
-  }
+	for _, ch := range strings.ToLower(word) {
+		switch {
+		case onePoint.MatchString(string(ch)):
+			score++
+		case twoPoint.MatchString(string(ch)):
+			score += 2
+		case threePoint.MatchString(string(ch)):
+			score += 3
+		case fourPoint.MatchString(string(ch)):
+			score += 4
+		case fivePoint.MatchString(string(ch)):
+			score += 5
+		case eightPoint.MatchString(string(ch)):
+			score += 8
+		case tenPoint.MatchString(string(ch)):
+			score += 10
+		}
+	}
 
-  return score
+	return score
 }

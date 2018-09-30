5e90a4842df63d5cb38e89d89832e8b3993a9612
Carl Cliver <carl@carls-air.lan>
2018-09-30 10:43:51 -0400

    Completed triangle exercise.

--- go/triangle/triangle.go
+++ go/triangle/triangle.go
@@ -1,5 +1,8 @@
 package triangle
 
+import "math"
+
+// Kind defines the type of a triangle.
 type Kind int
 
 const (
@@ -9,18 +12,23 @@ const (
 	Sca        // scalene
 )
 
+// KindFromSides determines what kind of triangle was passed in from the length of the sides.
 func KindFromSides(a, b, c float64) Kind {
-	var k Kind
+	switch {
+	case math.IsNaN(a) || math.IsNaN(b) || math.IsNaN(c) ||
+		math.IsInf(a, 0) || math.IsInf(b, 0) || math.IsInf(c, 0):
+		return NaT
+	case a <= 0 || b <= 0 || c <= 0:
+		return NaT
+	case ((a+b < c) || (b+c < a) || (a+c < b)):
+		return NaT
+	case a == b && b == c:
+		return Equ
+	case a == b || b == c || a == c:
+		return Iso
+	case a != b && b != c:
+		return Sca
+	}
 
-	if (a <= 0 || b <= 0 || c <= 0) || (a + b < c) || (b + c < a) || (a + c < b) {
-		k = NaT
-	} else if a == b && b == c {
-		k = Equ
-	} else if a == b || b == c || a == c {
-		k = Iso
-	} else if a != b && b != c {
-    k = Sca
-  }
-
-	return k
+	return NaT
 }

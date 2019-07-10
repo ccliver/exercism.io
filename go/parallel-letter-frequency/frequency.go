095cdb31cea27df5b79590ed9a2aabd204f6f09f
Carl Cliver <carl@carls-air-2.lan>
2019-07-09 22:26:05 -0400

    Go Track: Parallel Letter Frequency

---
+++ go/parallel-letter-frequency/frequency.go
@@ -0,0 +1,48 @@
+package letter
+
+import "sync"
+
+// FreqMap records the frequency of each rune in a given text.
+type FreqMap map[rune]int
+
+// Frequency counts the frequency of each rune in a given text and returns this
+// data as a FreqMap.
+func Frequency(s string) FreqMap {
+	m := FreqMap{}
+	for _, r := range s {
+		m[r]++
+	}
+	return m
+}
+
+// ConcurrentFrequency calls Frequency in a goroutine for each wordset in words, then combines and returns the result.
+func ConcurrentFrequency(words []string) FreqMap {
+	maps := FreqMap{}
+	mapBuf := make(chan FreqMap, len(words))
+	var wg sync.WaitGroup
+
+	callFrequency := func(str string) {
+		mapBuf <- Frequency(str)
+		wg.Done()
+	}
+
+	for _, s := range words {
+		wg.Add(1)
+		go callFrequency(s)
+	}
+	wg.Wait()
+
+	close(mapBuf)
+	for i := 0; i < len(words); i++ {
+		m := <-mapBuf
+		for k, v := range m {
+			if _, ok := maps[k]; ok {
+				maps[k] += v
+			} else {
+				maps[k] = v
+			}
+		}
+	}
+
+	return maps
+}
